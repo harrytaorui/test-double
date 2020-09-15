05b2d75e7a6c821e4c2ffd796ed4e3ce3874cec9
harrytaorui <rui.tao@thoughtworks.com>
2020-09-15 14:44:47 +0800

    [taorui]:securityCenter test updated

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -1,6 +1,7 @@
 package mock;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +16,7 @@ public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
+
 
     @Mock
     DoorPanel doorPanel;
