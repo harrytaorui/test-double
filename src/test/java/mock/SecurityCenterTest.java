1e6e034b331e4b73be975b5f553dc68a2c0eea65
harrytaorui <rui.tao@thoughtworks.com>
2020-09-15 12:36:08 +0800

    [taorui]:securityCenter mock test complete

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -3,11 +3,24 @@ package mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
+
+    @Mock
+    DoorPanel doorPanel;
+
+    @InjectMocks
+    SecurityCenter securityCenter;
 
     @BeforeEach
     public void setUp() {
@@ -15,5 +28,7 @@ public class SecurityCenterTest {
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
+        verify(doorPanel,times(1)).close();
     }
 }
